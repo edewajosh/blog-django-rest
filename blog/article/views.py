c3338cf4649d3b595290dea3bfc771d9b842254f
edewajosh <joshedewa@gmail.com>
2019-12-12 23:19:54 +0300

    Changed generic APIView for CRUD

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,11 +1,12 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-
-from .models import Article
+from rest_framework.generics import (
+    RetrieveAPIView, ListCreateAPIView, RetrieveUpdateAPIView)
+#from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from django.shortcuts import get_object_or_404
+from .models import Article, Author
 from .serializers import ArticleSerializer
 
 
-class ArticleView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class ArticleView(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
@@ -14,8 +15,17 @@ class ArticleView(ListModelMixin, CreateModelMixin, GenericAPIView):
             Author, id=self.request.data.get('author_id'))
         return serializer.save(author=author)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class SingleArticleView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def perform_create(self, serializer):
+        author = get_object_or_404(
+            Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
+
+
+class SingleArticleView(RetrieveUpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
